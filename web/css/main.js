95bba9dd6201335c4e617adb714086a88d590c12
FreeNetSZM <freenet.szm@seznam.cz>
2017-04-29 18:16:27 +0200

    Update main.js

--- web/css/main.js
+++ web/css/main.js
@@ -25,7 +25,7 @@ function ProduktID() {
                 '</div>\n' +
                 '<a href="http://www.zelenyzivot.sk/?a_aid=5551fa25d8bf9&a_bid=e470c965">\n' +
                   '<img src="/ZdravyZivot/web/images/zelenyzivot.png" target="_blank" id="IMG_ALI"/></a>\n' + 
-                '<div class="star-ratings-sprite"><span style="width:1%" id="STAR_' + id + '" class="star-ratings-sprite-rating"></span></div><div id="SCORE_' + id + '"></div>\n' +        
+                '<div class="star-ratings-sprite"><span style="width:99%" id="STAR_' + id + '" class="star-ratings-sprite-rating"></span></div><div id="SCORE_' + id + '"></div>\n' +        
               '</div>\n' +
           '</div>\n'
       document.getElementById("inner").appendChild(div);
